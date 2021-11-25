3f7aa17ec741d4bc6e9725926d1e6b5ebbc201f2
Azamat Toshtemirov <49729745+AzamatToshtemirov90@users.noreply.github.com>
2021-11-24 23:30:39 -0600

    ui xml changes

--- app/src/main/java/com/azamat/runnerapp/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/azamat/runnerapp/ui/fragments/RunFragment.kt
@@ -1,8 +1,16 @@
 package com.azamat.runnerapp.ui.fragments
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.azamat.runnerapp.R
+import com.azamat.runnerapp.ui.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class RunFragment : Fragment(R.layout.fragment_run){
+
+    //this is to get viewModel from Dagger
+    private val viewModel: MainViewModel by viewModels()
 
 }
--- app/src/main/java/com/azamat/runnerapp/ui/fragments/StatisticsFragment.kt
+++ app/src/main/java/com/azamat/runnerapp/ui/fragments/StatisticsFragment.kt
@@ -1,8 +1,16 @@
 package com.azamat.runnerapp.ui.fragments
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.azamat.runnerapp.R
+import com.azamat.runnerapp.ui.viewmodels.MainViewModel
+import com.azamat.runnerapp.ui.viewmodels.StatisticsViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class StatisticsFragment : Fragment(R.layout.fragment_statistics){
-    
+
+    //this is to get viewModel from Dagger
+    private val viewModel: StatisticsViewModel by viewModels()
+
 }
