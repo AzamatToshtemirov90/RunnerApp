7ce89cb5963e3ef8a4c1a17b989b6ba581fb4b3e
Azamat Toshtemirov <49729745+AzamatToshtemirov90@users.noreply.github.com>
2021-11-25 21:34:42 -0600

    navigation component is added

--- app/src/main/java/com/azamat/runnerapp/ui/MainActivity.kt
+++ app/src/main/java/com/azamat/runnerapp/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.azamat.runnerapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.azamat.runnerapp.R
@@ -16,5 +17,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
         bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener{_, destination, _ ->
+                when(destination.id){
+                    R.id.settingsFragment, R.id.statisticsFragment, R.id.runFragment ->
+                        bottomNavigationView.visibility = View.VISIBLE
+                    else -> bottomNavigationView.visibility = View.GONE
+
+                }
+            }
     }
 }
--- app/src/main/java/com/azamat/runnerapp/ui/fragments/SetupFragment.kt
+++ app/src/main/java/com/azamat/runnerapp/ui/fragments/SetupFragment.kt
@@ -1,8 +1,18 @@
 package com.azamat.runnerapp.ui.fragments
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.azamat.runnerapp.R
+import kotlinx.android.synthetic.main.fragment_setup.*
 
 class SetupFragment : Fragment(R.layout.fragment_setup){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        tvContinue.setOnClickListener {
+
+        }
+    }
 
 }
